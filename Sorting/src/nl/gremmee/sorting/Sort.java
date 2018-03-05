dbac0671872124d41657ba57695b504a21cec815
Gremmee <gremmee@gmail.com>
2018-03-05 14:15:36 +0100

    Sorting project
    Quicksort added

---
+++ Sorting/src/nl/gremmee/sorting/Sort.java
@@ -0,0 +1,22 @@
+package nl.gremmee.sorting;
+
+public class Sort {
+
+	public static void main(String[] args) {
+		
+		int[] unsorted = new int[10];
+		for (int i = 0; i < 10; i++){
+			unsorted[i] = i;
+		}
+		System.out.println("Unsorted");
+		System.out.println(unsorted);
+		System.out.println();
+//		QuickSort qsort = new Quicksort();
+
+//		int[] sorted = qsort.sort();
+		
+		System.out.println("Sorted");
+//		System.out.println(sorted..toString());
+	}   
+
+}
